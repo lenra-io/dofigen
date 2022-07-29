9ec267cbfee03e6c993466b8a6bdce142ba80b00
Thomas DA ROCHA <thomas.darocha@lenra.io>
2022-07-29 07:12:06 +0200

    feat: Manage envs

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,108 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::io::{self, BufRead};
+use std::{
+    collections::HashMap,
+    io::{self, BufRead},
+};
 
 #[derive(Serialize, Deserialize)]
 struct Artifact {
-    buider: String,
+    builder: String,
     source: String,
     destination: String,
 }
 
-// #[derive(Serialize, Deserialize)]
-// struct CopyFull {
-//     source: String,
-//     destination: Option<String>,
-//     chown: Option<String>,
-// }
+#[derive(Serialize, Deserialize)]
+struct CopyFull {
+    source: String,
+    destination: Option<String>,
+    chown: Option<String>,
+}
 
-// pub trait Copy {}
+// pub trait Copy {
+
+// }
 // impl Copy for String {}
 // impl Copy for CopyFull {}
+
+pub trait Stage {
+    fn image(&self) -> &str;
+    fn name(&self, position: i32) -> String;
+    fn envs(&self) -> Option<&HashMap<String, String>>;
+    fn additionnal_generation() {}
+
+    fn generate(&self, previous_builders: &mut Vec<String>) {
+        // println!("{}", serde_json::to_string(stage).unwrap());
+        let name = self.name(previous_builders.len().try_into().unwrap());
+        println!("\n# {}", name);
+        println!("FROM {} as {}", self.image(), name);
+
+        // Envs
+        if let Some(ref envs) = self.envs() {
+            print!("ENV ");
+            envs.iter()
+                .for_each(|(key, value)| println!("\\\n\t{}=\"{}\"", key, value));
+        }
+
+        previous_builders.push(name);
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct Builder {
     // Common part
     image: String,
+    envs: Option<HashMap<String, String>>,
     #[serde(rename = "rootScript")]
     root_script: Option<Vec<String>>,
-    script: Vec<String>,
+    script: Option<Vec<String>>,
     workdir: Option<String>,
-    copy: Option<Vec<String>>,
-    artifacts: Option<Vec<String>>,
+    add: Option<Vec<String>>,
+    artifacts: Option<Vec<Artifact>>,
     // Specific part
     name: Option<String>,
+}
+impl Stage for Builder {
+    fn image(&self) -> &str {
+        &self.image
+    }
+    fn name(&self, position: i32) -> String {
+        match self.name.as_ref() {
+            Some(name) => String::from(name),
+            None => format!("builder-{}", position),
+        }
+    }
+    fn envs(&self) -> Option<&HashMap<String, String>> {
+        self.envs.as_ref()
+    }
 }
 
 #[derive(Serialize, Deserialize)]
 struct Image {
     // Common part
     image: String,
+    envs: Option<HashMap<String, String>>,
     #[serde(rename = "rootScript")]
     root_script: Option<Vec<String>>,
-    script: Vec<String>,
+    script: Option<Vec<String>>,
     workdir: Option<String>,
+    add: Option<Vec<String>>,
+    artifacts: Option<Vec<Artifact>>,
     // Specific part
     builders: Option<Vec<Builder>>,
     ignore: Option<Vec<String>>,
 }
-
-pub trait Stage {}
-impl Stage for Image {}
-impl Stage for Builder {}
+impl Stage for Image {
+    fn image(&self) -> &str {
+        &self.image
+    }
+    fn name(&self, _position: i32) -> String {
+        String::from("runtime")
+    }
+    fn envs(&self) -> Option<&HashMap<String, String>> {
+        self.envs.as_ref()
+    }
+}
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
@@ -71,16 +124,13 @@ fn main() -> io::Result<()> {
 }
 
 fn generate_image(image: &Image) {
-    println!("{}", serde_json::to_string(image).unwrap());
-    image.builders.iter().for_each(builder {
-        generate_stage(&builder);
-    });
-    if let Some(builders) = image.builders {
-        println!("{} builders", builders.len().to_string())
+    println!("# syntax=docker/dockerfile:1.4");
+    let mut previous_builders = Vec::new();
+    if let Some(ref builders) = image.builders {
+        println!("{} builder(s)", builders.len().to_string());
+        builders
+            .iter()
+            .for_each(|builder| builder.generate(&mut previous_builders));
     }
-    generate_stage(image);
-}
-
-fn generate_stage<T: ?Stage + serde::Serialize>(stage: &T) {
-    println!("{}", serde_json::to_string(stage).unwrap());
+    image.generate(&mut previous_builders);
 }
