ddf0d172f1c9236e132e50b311717598167b5846
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-04-26 16:06:48 +0200

    fix: No error on unknown fields (#176)
    * fix: No error on unknown fields
    
    * test: Fix unit tests
    
    * style: Fix format
    
    * docs: Alias column

--- src/structs.rs
+++ src/structs.rs
@@ -3,21 +3,26 @@ use std::collections::HashMap;
 
 /** Represents the Dockerfile main stage */
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Default)]
+#[serde(deny_unknown_fields)]
 pub struct Image {
     // Common part
     #[serde(alias = "from")]
     pub image: String,
     pub user: Option<String>,
     pub workdir: Option<String>,
+    #[serde(alias = "env")]
     pub envs: Option<HashMap<String, String>>,
     pub artifacts: Option<Vec<Artifact>>,
+    #[serde(alias = "add")]
     pub adds: Option<Vec<String>>,
     pub root: Option<Root>,
     #[serde(alias = "run")]
     pub script: Option<Vec<String>>,
+    #[serde(alias = "cache")]
     pub caches: Option<Vec<String>>,
     // Specific part
     pub builders: Option<Vec<Builder>>,
+    #[serde(alias = "ignore")]
     pub ignores: Option<Vec<String>>,
     pub entrypoint: Option<Vec<String>>,
     pub cmd: Option<Vec<String>>,
@@ -33,12 +38,15 @@ pub struct Builder {
     pub image: String,
     pub user: Option<String>,
     pub workdir: Option<String>,
+    #[serde(alias = "env")]
     pub envs: Option<HashMap<String, String>>,
     pub artifacts: Option<Vec<Artifact>>,
+    #[serde(alias = "add")]
     pub adds: Option<Vec<String>>,
     pub root: Option<Root>,
     #[serde(alias = "run")]
     pub script: Option<Vec<String>>,
+    #[serde(alias = "cache")]
     pub caches: Option<Vec<String>>,
     // Specific part
     pub name: Option<String>,
@@ -48,6 +56,7 @@ pub struct Builder {
 pub struct Artifact {
     pub builder: String,
     pub source: String,
+    #[serde(alias = "target")]
     pub destination: String,
 }
 
@@ -55,6 +64,7 @@ pub struct Artifact {
 pub struct Root {
     #[serde(alias = "run")]
     pub script: Option<Vec<String>>,
+    #[serde(alias = "cache")]
     pub caches: Option<Vec<String>>,
 }
 
