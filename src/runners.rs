a75acb9ae359979c04a526ebea355547b14e4268
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-06-25 17:49:05 +0200

    test: Add some unit tests

--- src/runners.rs
+++ src/runners.rs
@@ -11,6 +11,9 @@ pub trait ScriptRunner {
     }
     fn add_script(&self, buffer: &mut String, uid: u16, gid: u16) {
         if let Some(script) = self.script() {
+            if script.is_empty() {
+                return;
+            }
             let mut lines: Vec<String> = script
                 .join(" &&\n")
                 .lines()
@@ -51,3 +54,99 @@ macro_rules! impl_ScriptRunner {
 }
 
 impl_ScriptRunner!(for Builder, Image, Root);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_script_with_script() {
+        let builder = Builder {
+            script: Some(vec!["echo Hello".to_string()]),
+            caches: None,
+            ..Default::default()
+        };
+        assert_eq!(builder.has_script(), true);
+    }
+
+    #[test]
+    fn test_has_script_without_script() {
+        let builder = Builder {
+            script: None,
+            caches: None,
+            ..Default::default()
+        };
+        assert_eq!(builder.has_script(), false);
+    }
+
+    #[test]
+    fn test_has_script_with_empty_script() {
+        let builder = Builder {
+            script: Some(vec![]),
+            caches: None,
+            ..Default::default()
+        };
+        assert_eq!(builder.has_script(), false);
+    }
+
+    #[test]
+    fn test_has_script_without_script_with_cache() {
+        let builder = Builder {
+            script: None,
+            caches: Some(vec!["/path/to/cache".to_string()]),
+            ..Default::default()
+        };
+        assert_eq!(builder.has_script(), false);
+    }
+
+    #[test]
+    fn test_add_script_with_script_and_caches() {
+        let mut buffer = String::new();
+        let builder = Builder {
+            script: Some(vec!["echo Hello".to_string()]),
+            caches: Some(vec!["/path/to/cache".to_string()]),
+            ..Default::default()
+        };
+        builder.add_script(&mut buffer, 1000, 1000);
+        assert_eq!(
+            buffer,
+            "RUN \\\n    --mount=type=cache,sharing=locked,uid=1000,gid=1000,target=/path/to/cache \\\n    echo Hello\n"
+        );
+    }
+
+    #[test]
+    fn test_add_script_with_script_without_caches() {
+        let mut buffer = String::new();
+        let builder = Builder {
+            script: Some(vec!["echo Hello".to_string()]),
+            caches: None,
+            ..Default::default()
+        };
+        builder.add_script(&mut buffer, 1000, 1000);
+        assert_eq!(buffer, "RUN \\\n    echo Hello\n");
+    }
+
+    #[test]
+    fn test_add_script_without_script() {
+        let mut buffer = String::new();
+        let builder = Builder {
+            script: None,
+            caches: None,
+            ..Default::default()
+        };
+        builder.add_script(&mut buffer, 1000, 1000);
+        assert_eq!(buffer, "");
+    }
+
+    #[test]
+    fn test_add_script_with_empty_script() {
+        let mut buffer = String::new();
+        let builder = Builder {
+            script: Some(vec![]),
+            caches: None,
+            ..Default::default()
+        };
+        builder.add_script(&mut buffer, 1000, 1000);
+        assert_eq!(buffer, "");
+    }
+}
